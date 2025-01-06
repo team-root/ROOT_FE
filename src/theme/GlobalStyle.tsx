20228e2aeb2347325585c3a586fb0f65126af752
박지연 <163213733+j02on@users.noreply.github.com>
2025-01-06 11:43:18 +0900

    SETTING :: Font 적용
    GlobalStyle에 Font를 적용하였습니다.

--- src/theme/GlobalStyle.tsx
+++ src/theme/GlobalStyle.tsx
@@ -1,11 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "PretendardVariable";
+    src: url('./"PretendardVariable.ttf');
+    font-weight: 100 900;
+  }
   * {
       margin: 0;
       border: 0;
       padding: 0;
       box-sizing: border-box;
+      font-family: "PretendardVariable";
   }
   menu, ol, ul {
     list-style: none;
