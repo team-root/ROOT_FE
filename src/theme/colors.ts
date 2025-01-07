c7a4a87bf9f7d2a428ab1c91e0662c838e753bcc
kimsiwoo <ksw98035213@gmail.com>
2025-01-07 11:48:51 +0900

    FEAT :: button component 수정

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,31 +1,25 @@
-
-
-
 export const colors = {
   gray: {
-    100: '#fff',
-    200: '#E4E6EB',
-    300: '#C6C8CC',
-    400: '#7A7C80',
-    500: '#494A4C',
-    550: '#242424',
-    600: '#1C1C1C',
-    700: '#000',
+    100: "#fff",
+    200: "#E4E6EB",
+    300: "#C6C8CC",
+    400: "#7A7C80",
+    500: "#494A4C",
+    550: "#242424",
+    600: "#1C1C1C",
+    700: "#000",
   },
 
   main: {
-    100: '#88A0FC',
-    200: '#6686FF',
+    100: "#88A0FC",
+    200: "#6686FF",
     300: {
-      from: '#D5A0FF',
-      to: '#9CE6FE',
-      direction: 'to right',
+      from: "#D5A0FF",
+      to: "#9CE6FE",
+      direction: "to right",
     },
-    400: '#BFCCFF',
+    400: "#BFCCFF",
   },
 
-  error: '#F54B31',
-
+  error: "#F54B31",
 } as const;
-
-export type Gradient = typeof colors["main"]["300"];
