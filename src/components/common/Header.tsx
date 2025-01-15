b277fc81aee82efb54abf423db586d9a98cf975e
kimsiwoo <ksw98035213@gmail.com>
2025-01-15 09:28:07 +0900

    FEAT :: 컬러시스템 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,24 +3,36 @@ import { colors, font } from "../../theme";
 import { useState } from "react";
 import { LoginBtn } from "../header";
 import { logo } from "../../assets";
+import { useLocation } from "react-router-dom";
 
 export const Header = () => {
   const [isLogin, setIsLogin] = useState<boolean>(true);
+  const location = useLocation();
+
+  const navItems = [
+    { label: "학생봉사 시간 조회/부여", path: "/hours" },
+    { label: "봉사활동 신청 조회/생성", path: "/volunteer-posts" },
+    { label: "봉사 일정 확인", path: "/schedules" },
+    { label: "알림 생성", path: "/notifications" },
+  ];
   return (
     <HeaderContainer>
       <LogoContainer>
         <img src={logo} alt="logo" />
         {isLogin && (
           <NavContainer>
-            <Nav>학생봉사 시간 조회/부여</Nav>
-            <Nav>봉사활동 신청 조회/생성</Nav>
-            <Nav>봉사 일정 확인</Nav>
-            <Nav>알림 생성</Nav>
+            {navItems.map((nav) => (
+              <Nav key={nav.path} isActive={location.pathname === nav.path}>
+                {nav.label}
+              </Nav>
+            ))}
           </NavContainer>
         )}
       </LogoContainer>
       <LoginContainer>
-        {isLogin && <Nav>마이페이지</Nav>}
+        {isLogin && (
+          <Nav isActive={location.pathname === "/mypage"}>마이페이지</Nav>
+        )}
         <LoginBtn isLogined={isLogin}></LoginBtn>
       </LoginContainer>
     </HeaderContainer>
@@ -51,7 +63,16 @@ const NavContainer = styled.nav`
   gap: 72px;
 `;
 
-const Nav = styled.div`
+const Nav = styled.div<{ isActive: boolean }>`
   font: ${font.Body3};
-  color: ${colors.gray[100]}; //path에 따라 색상변경 기능 추가
+  ${({ isActive }) =>
+    isActive
+      ? `
+    background: linear-gradient(to right, ${colors.main[300].from}, ${colors.main[300].to});
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  `
+      : `
+    color: ${colors.gray[100]};
+  `};
 `;
