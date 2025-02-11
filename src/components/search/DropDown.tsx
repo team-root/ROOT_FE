837fbe8e382608cc2fd880e7ddd01e2f04d84fc4
kimsiwoo <ksw98035213@gmail.com>
2025-02-11 10:43:25 +0900

    FEAT :: dropdown 구현

---
+++ src/components/search/DropDown.tsx
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import { colors, font } from "../../theme";
+import { useState } from "react";
+import { DownArrow } from "../../assets";
+
+export const DropDown = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [selected, setSelected] = useState<string>("학년순");
+
+  const dropdowns: string[] = [
+    "학년순",
+    "1-1",
+    "1-2",
+    "1-3",
+    "1-4",
+    "2-1",
+    "2-2",
+    "2-3",
+    "2-4",
+    "3-1",
+    "3-2",
+    "3-3",
+    "3-4",
+  ];
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+    console.log("123");
+  };
+
+  const selectDropdown = (x: string) => {
+    setSelected(x);
+    setIsDropdownOpen(false);
+  };
+
+  return (
+    <>
+      <Container onClick={toggleDropdown}>
+        <DropdownText>{selected}</DropdownText>
+        <img src={DownArrow} alt="버튼" />
+      </Container>
+      {isDropdownOpen && (
+        <DropdownMenu>
+          {dropdowns
+            .filter((x) => x !== selected)
+            .map((item) => (
+              <DropdownItem key={item} onClick={() => selectDropdown(item)}>
+                <DropdownText>{item}</DropdownText>
+              </DropdownItem>
+            ))}
+        </DropdownMenu>
+      )}
+    </>
+  );
+};
+
+const Container = styled.div`
+  width: 160px;
+  height: 50px;
+  border: 1px solid ${colors.gray[400]};
+  border-radius: 10px;
+  background-color: ${colors.gray[550]};
+  display: flex;
+  align-items: center;
+  padding-left: 55px;
+  gap: 28px;
+  cursor: pointer;
+`;
+
+const DropdownText = styled.p`
+  ${font.Body4};
+  color: #fff;
+`;
+
+const DropdownMenu = styled.div`
+  width: 160px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.gray[550]};
+  border-radius: 10px;
+  border: 1px solid ${colors.gray[400]};
+`;
+
+const DropdownItem = styled.div`
+  width: 158px;
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.gray[550]};
+  &:hover {
+    background-color: ${colors.main[100]};
+  }
+`;
