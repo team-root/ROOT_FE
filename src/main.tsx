e8e11a2d36e3162f3d75351e28c9f918e372c4ec
박지연 <163213733+j02on@users.noreply.github.com>
2025-02-12 08:53:00 +0900

    SETTING :: API

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')! as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>
+  </QueryClientProvider>
 );
